c300db0bfc369dd7a05154b1971ac65e2bfa0427
rickyputrasardika22 <nadim@mhs.mdp.ac.id>
2022-10-06 15:05:51 +0700

    crud sql

---
+++ myFirstCRUDSQL/app/src/main/java/com/if5b/myfirstcrudsql/MainActivity.java
@@ -0,0 +1,26 @@
+package com.if5b.myfirstcrudsql;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+public class MainActivity extends AppCompatActivity {
+    FloatingActionButton fabTambah;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+    public void bukaActivityTambah(View view) {
+        startActivity(new Intent(MainActivity.this, tambah.class));
+    }
+
+}
