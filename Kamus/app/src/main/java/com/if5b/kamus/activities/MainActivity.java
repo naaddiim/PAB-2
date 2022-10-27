f7dc6f99d2bc6680726b51b2bd7ed7de933bde5a
naaddiim <nadim@mhs.mdp.ac.id>
2022-10-27 13:15:52 +0700

    tes commit

--- Kamus/app/src/main/java/com/if5b/kamus/activities/MainActivity.java
+++ Kamus/app/src/main/java/com/if5b/kamus/activities/MainActivity.java
@@ -22,6 +22,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        //aa
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
